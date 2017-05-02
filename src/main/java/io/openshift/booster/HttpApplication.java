7bb3e96369041db6078a44876c8f7337dd005f29
Alexey Kazakov <igelsss+osio@gmail.com>
2017-05-02 17:39:03 +0000

    Fixed the bug

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  private static final String template = "Hello, %s from openshift.io!";
+  private static final String template = "Hello, %s from openshift.io!!!";
 
   @Override
   public void start(Future<Void> future) {
